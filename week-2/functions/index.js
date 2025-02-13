81396d2cfc3e3514e509dbb29664c3e5f932117e
Ivy Ditzenberger <emditzen@iu.edu>
2025-02-13 15:45:31 -0500

    week-2 finish

--- week-2/functions/index.js
+++ week-2/functions/index.js
@@ -1,16 +1,11 @@
-// this also works 
+ function calcTriArea({base, height} = {}) {
+const baseValue = base || 1;
+const heightValue = height || 1;
 
-// // function calcTriArea({base, height} = {}) {
-// const baseValue = base || 1;
-// const heightValue = height || 1;
-
-// return 0.5 * baseValue * heightValue;
-// }
-
-function calcTriArea(options = {}) {
-    const { base = 1, height = 1} = options;
-    return 0.5 * base * height;
+return 0.5 * baseValue * heightValue;
 }
+
+calcTriArea.props = { base: 1, height: 1};
 
 function calcSquareArea(length, width) {
     return length * width;
--- week-2/index.js
+++ week-2/index.js
@@ -1,6 +1,13 @@
+// pre-built
+const readline = require("readline")
+
+// installed dependencies
+
+//custom modules
+
 const mathFuncs = require("./functions");
 
-const {calcTriArea} = mathFuncs
+const { calcTriArea,} = mathFuncs
 
 const ans1 = calcTriArea({
     base: 3, height: 7});
@@ -12,3 +19,26 @@ console.log(ans2); // OUTPUT: 1
 
 const ans3 = calcTriArea();
 console.log(ans3); // OUTPUT: 0.5
+
+const app = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+app.question(`Enter the height of the triangle:
+    height: (${calcTriArea.props.height})`, 
+    function (userHeight) {
+        console.log(userHeight);
+
+        app.question(`Enter the base of the triangle:
+    base: (${calcTriArea.props.base})`,
+     function(userBase) {
+        const userAns = calcTriArea({base: userBase,
+            height: userHeight});
+        console.log(`User Ans: ${userAns}`);
+        app.close();
+    }
+    );
+ }
+);
+
